a7ccf7fc5b106f818a92b6bcb47592e53b1cf2f8
Kevin Song <kevinmnm2010@gmail.com>
2022-10-15 20:40:37 -0400

    change to env

--- js/etc/ENV.js
+++ js/etc/ENV.js
@@ -1,5 +1,5 @@
 export const APP_ENV = "development"; // Or "production".
 
-export const SERVER_URL = (APP_ENV === "production") ? "" : "http://localhost:8081";
+export const SERVER_URL = (APP_ENV === "production") ? "https://kevsong.com" : "http://localhost:8081";
 export const MESSAGE_SEND_ROUTE = "msg-send";
 export const MESSAGE_GET_ROUTE = "msg-get";
